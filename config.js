051a7d461f1afde464522378bf21628be2c009bc
Dau Hoai <hoailuoihoc@gmail.com>
2022-08-18 19:55:38 +0700

    monquanhonhone

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Ơ, Hằng đến rồi à',
+    introDesc: `Ummm, tớ có điều này muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Dạ',
+    title: 'Hằng thích tớ đúng hông🥰 ',
+    desc: 'Cậu không trả lời hoặc thoát ra là tớ bùn lắm đó nha <3 ',
+    btnYes: 'Sao cậu biết hay dợ 💖💖💖',
+    btnNo: 'Không, tớ làm gì thích cậu',
+    question:'Hì, vì sao lại thích tớ nè',
+    reasonPlaceholder: 'Tớ đẹp trai lắm hở',
+    btnReply: 'Gởi tới cậu ! 💌💌',
+    reply: 'Tại vì cậu đẹp trai, học giỏi và cậu chính là cậu nữa<3 chàng trai của tớ....Gửi nè.. 😊😊',
+    mess: 'Thôi, tớ biết mà.Tớ cũng iu cậu::: Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tớ nha, tớ chờ cậu nãy giờ. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/hoaiit2k3'
+}
